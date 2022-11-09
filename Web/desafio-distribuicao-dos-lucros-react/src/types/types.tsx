376915bee113e2439b53db6fe9fd522b17b1e1ca
Lukas A. Santiago <lukas.asant@gmail.com>
2022-11-09 10:57:27 -0300

    front feature: Declarando tipos

--- Web/desafio-distribuicao-dos-lucros-react/src/types/types.tsx
+++ Web/desafio-distribuicao-dos-lucros-react/src/types/types.tsx
@@ -1,4 +1,4 @@
-export type ConfiguracaoCalculo = {
+export type ConfiguracaoCalculo = BaseModel & {
   id: number
   creationDate: string
   updatedDate: string
@@ -7,15 +7,55 @@ export type ConfiguracaoCalculo = {
   salarioMinimo: number
 }
 
-export type Funcionario = {
-  id: number
-  creationDate: Date
-  updatedDate: Date
-  ativo: boolean
+export type Funcionario = BaseModel & {
   matricula: string
   nome: string
   areaAtuacao: number
   cargo: number
   salarioBruto: number
   dataAdmissao: Date
+}
+
+export type RelatorioDistribuicao = BaseModel & {
+  totalDisponibilizado: number
+  totalDistribuido: number
+  salarioMinimo: number
+  saldoDisponibilizadoDistribuido: number
+  relatorioDistribuicaoFuncionario: RelatorioDistribuicaoFuncionario[]
+  totalFuncionarios: number
+  relatorioDistribuicaoPeso: RelatorioDistribuicaoPeso[]
+}
+
+export type RelatorioDistribuicaoFuncionario = BaseModel & {
+  matricula: string
+  nome: string
+  areaAtuacao: number
+  dataAdmissao: string
+  cargo: number
+  salarioBruto: number
+  valorDisponibilizado: number
+  valorTotal: number
+  relatorioDistribuicaoId: number
+  relatorioDistribuicao: RelatorioDistribuicao
+}
+
+export type RelatorioDistribuicaoPeso = Peso & {
+  relatorioDistribuicaoId: number
+  relatorioDistribuicao: RelatorioDistribuicao | null
+}
+
+export type Peso = BaseModel & {
+  tipoPeso: string
+  valor: number
+  nome: string
+  valorMaximo: number
+  valorMinimo: number
+  funcionarios: Funcionario[]
+}
+
+export type BaseModel = {
+  id: number
+  creationDate: Date | string
+  updatedDate: Date | string
+  ativo: boolean
 }
